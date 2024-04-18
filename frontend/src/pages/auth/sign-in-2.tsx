6e87bd854e3c32ba6e28ef6ce5c5d4cf03ccd141
Alexander C <alex@machineacuity.com>
2024-04-18 08:24:41 +0000

    Update logo, add eslint, reorganise gitignore

--- frontend/src/pages/auth/sign-in-2.tsx
+++ frontend/src/pages/auth/sign-in-2.tsx
@@ -1,4 +1,5 @@
 import { SignInForm } from './components/sign-in-form'
+import LogoDiscreteSvg from '@/assets/logo-discrete.svg'
 import { Card } from '@/components/ui/card'
 
 export default function SignIn2() {
@@ -7,18 +8,7 @@ export default function SignIn2() {
       <div className='container grid h-svh flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0'>
         <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8'>
           <div className='mb-4 flex items-center justify-center'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='mr-2 h-6 w-6'
-            >
-              <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-            </svg>
+            <img src={LogoDiscreteSvg} alt='Logo' className='mr-2' />
             <h1 className='text-xl font-medium'>Shadcn Admin</h1>
           </div>
           <Card className='p-6'>
