f5fe8f3fc177c289d83dbc378679accb28447aef
Alexander C <alex@machineacuity.com>
2024-04-19 17:17:23 +0000

    Add ToDo

---
+++ migration/src/m20240101_000001_todos.rs
@@ -0,0 +1,49 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                table_auto(Todos::Table)
+                    .col(pk_auto(Todos::Id))
+                    .col(string(Todos::Text))
+                    .col(integer(Todos::UserId))
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-todos-users")
+                            .from(Todos::Table, Todos::UserId)
+                            .to(Users::Table, Users::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Todos::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Todos {
+    Table,
+    Id,
+    Text,
+    UserId,
+    
+}
+
+
+#[derive(DeriveIden)]
+enum Users {
+    Table,
+    Id,
+}
