3c184f64e2694d3d6883470c506ac7820469ff99
Alexander C <alex@machineacuity.com>
2024-05-02 19:42:30 +0000

    Print body of proxied requests in vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,9 +7,36 @@ import {
   // @ts-ignore
 } from './src/data/branding-strings'
 import react from '@vitejs/plugin-react-swc'
+import chalk from 'chalk'
 import path from 'path'
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars'
+
+const getPostData = (req) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  return new Promise((resolve, _reject) => {
+    let data = ''
+    req.on('data', (chunk) => {
+      data += chunk.toString()
+    })
+
+    req.on('end', () => {
+      if (!data) {
+        resolve({})
+      }
+
+      resolve(data)
+    })
+  })
+}
+
+let current_request_seq_no = 0
+const getRequestSeqNo = () => {
+  current_request_seq_no++
+  if (current_request_seq_no > 999) current_request_seq_no = 1
+
+  return chalk.white(('' + current_request_seq_no).padStart(3))
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -38,17 +65,26 @@ export default defineConfig({
         configure: (proxy, _options) => {
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
           proxy.on('error', (err, _req, _res) => {
-            console.error('proxy error', err)
+            console.error('PROXY ERROR', err)
           })
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url)
+          proxy.on('proxyReq', async (_proxyReq, req, _res) => {
+            const req_seq_no = getRequestSeqNo()
+            req['req_seq_no'] = req_seq_no
+            const requestBody = await getPostData(req)
+            console.log(
+              chalk.gray.bgBlack('[API ' + req_seq_no + ']') + '-->',
+              req.method,
+              req.url,
+              requestBody
+            )
           })
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
           proxy.on('proxyRes', (proxyRes, req, _res) => {
             console.log(
-              'Received Response from the Target:',
+              chalk.gray.bgBlack('[API ' + req['req_seq_no'] + ']'),
               proxyRes.statusCode,
+              req.method,
               req.url
             )
           })
